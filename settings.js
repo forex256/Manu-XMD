ef8b4ebed6d85ea6c6c0118b8a99ef3fec33bf42
forex256 <kavishasithumina074@gmail.com>
2025-02-27 08:47:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&oe8yjCIB#BfsxEoboRYhIc7ta3iQ_Gs3hyJLK1BbqG7U_XAK_OuA' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
